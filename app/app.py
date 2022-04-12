cbccf86a5f93b0463cb3a60400cda9fabed535c6
Aniket Maurya <theaniketmaurya@gmail.com>
2022-04-12 16:01:58 +0530

    first commit

---
+++ app/app.py
@@ -0,0 +1,21 @@
+from typing import Dict, List
+
+from examples.research_app.jupyter import JupyterWork
+from lightning import LightningApp, LightningFlow
+
+
+class Flow(LightningFlow):
+    def __init__(self) -> None:
+        super().__init__()
+        self.jupyter = JupyterWork()
+
+    def run(self) -> None:
+        self.jupyter.run()
+
+    def configure_layout(self) -> List[Dict]:
+        return [
+            {"name": "Jupyter", "content": self.jupyter.exposed_url("jupyter")},
+        ]
+
+
+app = LightningApp(Flow())
---
+++ app/jupyter.py
@@ -0,0 +1,61 @@
+import logging
+import os
+import subprocess
+from pathlib import Path
+
+from lightning import LightningWork
+
+logger = logging.getLogger(__name__)
+
+
+class JupyterWork(LightningWork):
+    def __init__(self, host="127.0.0.1", port=8888):
+        super().__init__(exposed_ports={"jupyter": 8888})
+        self._proc = None
+        self.host = host
+        self.port = port
+        self.pid = None
+        self.exit_code = None
+
+    def run(self):
+
+        jupyter_notebook_config_path = Path.home() / ".jupyter/jupyter_notebook_config.py"
+
+        os.remove(jupyter_notebook_config_path)
+
+        cmd = "jupyter notebook --generate-config"
+
+        with subprocess.Popen(
+            cmd.split(" "), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=0, close_fds=True
+        ) as proc:
+            self._proc = proc
+            self.pid = proc.pid
+            if proc.stdout:
+                with proc.stdout:
+                    for line in iter(proc.stdout.readline, b""):
+                        logger.info("%s", line.decode().rstrip())
+
+            self.exit_code = proc.wait()
+            if self.exit_code != 0:
+                raise Exception(self.exit_code)
+
+        with open(jupyter_notebook_config_path, "a") as f:
+            f.write(
+                """c.NotebookApp.tornado_settings = {'headers': {'Content-Security-Policy': "frame-ancestors * 'self' "}}"""  # noqa E501
+            )
+
+        cmd = "jupyter lab"
+
+        with subprocess.Popen(
+            cmd.split(" "), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=0, close_fds=True
+        ) as proc:
+            self._proc = proc
+            self.pid = proc.pid
+            if proc.stdout:
+                with proc.stdout:
+                    for line in iter(proc.stdout.readline, b""):
+                        logger.info("%s", line.decode().rstrip())
+
+            self.exit_code = proc.wait()
+            if self.exit_code != 0:
+                raise Exception(self.exit_code)
