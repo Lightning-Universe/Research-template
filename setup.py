217e82d6114773988540a82c9c76849b18d3354d
Stan Biryukov <stan0625@uw.edu>
2022-04-12 13:30:28 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("research_app/requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="research_app",
+    version="0.0.1",
+    description="Research App",
+    author="Grid.ai",
+    packages=setuptools.find_packages(where="research_app*"),
+    install_requires=[
+        requirements
+    ],
+    include_package_data=True,
+    extras_require={
+        "dev": ["ipdb==0.13.9", "black==22.3.0", "isort==5.10.1"],
+    },
+    python_requires=">=3.9.1",
+)
