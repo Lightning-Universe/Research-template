135be07a9e46162b3f320edcf805b788a789305d
Aniket Maurya <theaniketmaurya@gmail.com>
2022-04-15 15:56:42 +0530

    parameterized app

--- research_app/app.py
+++ research_app/app.py
@@ -1,13 +1,23 @@
-from typing import Dict, List
+from typing import Dict, List, Optional
 
-from jupyter_works import JupyterWork
 from gradio_works import GradioWork
+from jupyter_works import JupyterWork
 from lightning import LightningApp, LightningFlow
 
 
 class Flow(LightningFlow):
-    def __init__(self, jupyter_port=8888, gradio_port=8889) -> None:
+    def __init__(
+            self,
+            paper: Optional[str] = None,
+            blog: Optional[str] = None,
+            github: Optional[str] = None,
+            jupyter_port=8888,
+            gradio_port=8889,
+    ) -> None:
         super().__init__()
+        self.paper = paper
+        self.blog = blog
+        self.github = github
         self.jupyter = JupyterWork(port=jupyter_port)
         self.gradio = GradioWork(port=gradio_port)
 
@@ -15,12 +25,27 @@ class Flow(LightningFlow):
         self.gradio.run()
         self.jupyter.run()
 
+    @property
+    def vscode(self):
+        return f"https://vscode.dev/{self.github}"
+
     def configure_layout(self) -> List[Dict]:
-        return [
-            {"name": "Arxiv", "content": "https://arxiv.org/pdf/1811.06965.pdf"},
-            {"name": "Jupyter", "content": self.jupyter.exposed_url("jupyter")},
+        tabs = []
+        if self.paper:
+            tabs.append({"name": "Paper", "content": self.paper})
+        if self.blog:
+            tabs.append({"name": "Blog", "content": self.blog})
+        if self.github:
+            tabs.append({"name": "vscode", "content": self.vscode})
+        tabs.append(
             {"name": "Deployment", "content": self.gradio.exposed_url("gradio")},
-        ]
+        )
+        return tabs
 
 
-app = LightningApp(Flow())
+if __name__ == "__main__":
+    paper = "https://arxiv.org/pdf/1811.06965.pdf"
+    blog = "https://openai.com/blog/clip/"
+    github = "https://github.com/mlfoundations/open_clip"
+
+    app = LightningApp(Flow(paper=paper, blog=blog, github=github))
--- research_app/gradio_works.py
+++ research_app/gradio_works.py
@@ -8,15 +8,31 @@ logger = logging.getLogger(__name__)
 
 
 def predict(name):
-    return "Hello " + name + "!! Replace `predict` func with prediction function for your model."
+    return (
+        "Hello "
+        + name
+        + "!! Replace `predict` func with prediction function for your model."
+    )
 
 
 class GradioWork(LightningWork):
-    def __init__(self, host="0.0.0.0", port=8889, ):
+    def __init__(
+        self,
+        host="0.0.0.0",
+        port=8889,
+    ):
         super().__init__(exposed_ports={"gradio": port}, blocking=False)
         self.host = host
         self.port = port
 
-    def run(self, gradio_fn: Callable = predict, inputs="text", outputs="text", **interface_kwargs):
-        iface = gr.Interface(fn=gradio_fn, inputs=inputs, outputs=outputs, **interface_kwargs)
+    def run(
+        self,
+        gradio_fn: Callable = predict,
+        inputs="text",
+        outputs="text",
+        **interface_kwargs
+    ):
+        iface = gr.Interface(
+            fn=gradio_fn, inputs=inputs, outputs=outputs, **interface_kwargs
+        )
         iface.launch(server_port=self.port)
