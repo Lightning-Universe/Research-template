3a30d6ad683b4bee0a0402cbd2dd5ebf820324c2
Stan Biryukov <stan0625@uw.edu>
2022-04-21 19:11:03 -0500

    adds `PosterWork` which serves mkposter generated html from markdown poster/poster.md

---
+++ research_app/components/poster_work.py
@@ -0,0 +1,28 @@
+import logging
+import subprocess
+
+from lightning import LightningWork
+from mkposters import mkposter
+
+from research_app.utils import get_random_port
+
+logger = logging.getLogger(__name__)
+
+
+class PosterWork(LightningWork):
+    """
+    :param port: Port address for app.
+    :param blocking: Whether the Work is blocking
+    """
+
+    def __init__(
+        self,
+        port=8000,
+        blocking=False,
+    ):
+        port = port or get_random_port()
+        super().__init__(exposed_ports={"poster": port}, blocking=blocking)
+        self.port = port
+
+    def run(self):
+        mkposter("research_app/poster")
