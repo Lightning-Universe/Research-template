1d99090d00514c82c844caf41bc8a97309e14293
Stan Biryukov <stan0625@uw.edu>
2022-04-27 14:44:46 -0500

    add args to updated `mkposter`

--- research_app/components/poster_work.py
+++ research_app/components/poster_work.py
@@ -17,11 +17,19 @@ class PosterWork(LightningWork):
     def __init__(
         self,
         port=8000,
+        code_style="github",
+        background_color="#F6F6EF",
         blocking=False,
     ):
         port = port or get_random_port()
         super().__init__(exposed_ports={"poster": port}, blocking=blocking)
         self.port = port
+        self.code_style = code_style
+        self.background_color = background_color
 
     def run(self):
-        mkposter("research_app/poster")
+        mkposter(
+            datadir="research_app/poster",
+            background_color=self.background_color,
+            code_style=self.code_style,
+        )
