2bdac0f54e0019ba6a9ddd2738d81852108a51a1
Stan Biryukov <stan0625@uw.edu>
2022-04-26 21:37:27 -0500

    drop post-install logic now that dart-sass install is upstream in mkposters

--- setup.py
+++ setup.py
@@ -1,36 +1,6 @@
 import subprocess
 
 import setuptools
-from setuptools.command.install import install
-
-
-class PostBuild(install):
-    """Run _post_install function after install is complete."""
-
-    def run(self):
-        install.run(self)
-        self.execute(
-            _post_install, (self.install_lib,), msg="Running post install task"
-        )
-
-
-def _post_install(dir):
-    cmd = "/bin/bash bash_scripts/build_mkposters.sh"
-    with subprocess.Popen(
-        cmd.split(" "),
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        bufsize=0,
-        close_fds=True,
-    ) as proc:
-        if proc.stdout:
-            with proc.stdout:
-                for line in iter(proc.stdout.readline, b""):
-                    print(line.decode().rstrip())
-        exit_code = proc.wait()
-        if exit_code != 0:
-            raise Exception(exit_code)
-
 
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
@@ -51,6 +21,5 @@ setuptools.setup(
             "pre-commit==2.18.1",
         ],  # E501
     },
-    cmdclass={"install": PostBuild},
     python_requires=">=3.8",
 )
