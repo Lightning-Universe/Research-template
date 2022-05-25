6b7b4c30dabf118f4ac42b9799e3d2f35b47e1e9
Aniket Maurya <theaniketmaurya@gmail.com>
2022-05-26 01:09:28 +0530

    update

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     paper = "https://arxiv.org/pdf/2103.00020"
     blog = "https://openai.com/blog/clip/"
     github = "https://github.com/openai/CLIP"
-    wandb = "https://wandb.ai/dalle-mini/dalle-mini/runs/3r9ew7qt?workspace="
+    wandb = "https://wandb.ai/cceyda/flax-clip/runs/wlad2c2p?workspace=user-aniketmaurya"
 
     app = LightningApp(
         ResearchApp(
