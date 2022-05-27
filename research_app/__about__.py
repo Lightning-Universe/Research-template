36f9117f257dd0ab3b66fec0b093fc31a7d6d87e
Aniket Maurya <theaniketmaurya@gmail.com>
2022-05-27 20:11:19 +0530

    update about

--- research_app/__about__.py
+++ research_app/__about__.py
@@ -1,10 +1,10 @@
-__version__ = "0.0.2"
+__version__ = "0.0.1"
 __author__ = "PyTorchLightning et al."
-__author_email__ = "name@pytorchlightning.ai"
-__license__ = "TBD"
+__author_email__ = "aniket@grid.ai"
+__license__ = "Apache 2.0"
 __copyright__ = f"Copyright (c) 2021-2022, {__author__}."
 __homepage__ = "https://github.com/PyTorchLightning/lightning-template-research-app"
-__docs__ = "Project x"
+__docs__ = "Quickest way to share everything about your research within a single app"
 __long_doc__ = """
 Research Poster App helps Authors and Readers to publish and view research, code, experiment reports,
 articles or any resource within the same app.
