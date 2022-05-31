b8c269e80de01a2eb72bbd2f0c35252e9c6b47f7
Aniket Maurya <theaniketmaurya@gmail.com>
2022-05-31 11:43:43 +0530

    fix

--- research_app/__about__.py
+++ research_app/__about__.py
@@ -4,10 +4,15 @@ __author_email__ = "aniket@grid.ai"
 __license__ = "Apache 2.0"
 __copyright__ = f"Copyright (c) 2021-2022, {__author__}."
 __homepage__ = "https://github.com/PyTorchLightning/lightning-template-research-app"
-__docs__ = "Share your paper "bundled" with the arxiv link, poster, live jupyter notebook, interactive demo to try the model and more!"
+__docs__ = (
+    "Share your paper 'bundled' with the arxiv link, poster, live jupyter notebook,"
+    "interactive demo to try the model and more!"
+)
 __long_doc__ = """
-Use this app to share your research paper results. This app lets you connect a blogpost, arxiv paper, jupyter notebook, and even have an interactive demo for people to play with the model.
-This app also allows industry practicioners to productionize your work by adding inference components (sub 1ms inference time), data pipelines, etc.
+Use this app to share your research paper results. This app lets you connect a blogpost, arxiv paper, jupyter notebook,
+and even have an interactive demo for people to play with the model.
+This app also allows industry practitioners to productionize your work by adding inference components
+(sub 1ms inference time), data pipelines, etc.
 """
 
 __all__ = [
