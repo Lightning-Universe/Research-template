ba21099dcae5551f4583c6da31b8b51244f6c14c
Aniket Maurya <theaniketmaurya@gmail.com>
2022-06-15 03:58:29 +0530

    fix nbconvert Race condition (#82)
    * fix nbconvert Race condition
    
    * update
    
    * refactor git clone

--- research_app/utils.py
+++ research_app/utils.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import subprocess
+import tempfile
 from pathlib import Path
 
 from rich.logging import RichHandler
@@ -16,16 +17,10 @@ def notebook_to_html(path: str) -> str:
     if not os.path.exists(path):
         raise FileNotFoundError(f"Can't convert notebook to html, path={path} not found!")
 
-    folder = os.path.dirname(path)
-    converted_html = path.replace("ipynb", "html")
-    renamed_html = folder + "/index.html"
-    if os.path.exists(converted_html) or os.path.exists(renamed_html):
-        logger.info(f"Skipping nbconvert, {path} converted into html!")
-    else:
-        command = f"jupyter nbconvert --to html {path}"
-        subprocess.run(command, shell=True)
-        os.rename(converted_html, renamed_html)
-    return folder
+    tempdir = tempfile.mkdtemp()
+    command = f"jupyter nbconvert --to html {path} --output-dir='{tempdir}' --output index.html"
+    subprocess.run(command, shell=True)
+    return tempdir
 
 
 def clone_repo(url: str) -> str:
@@ -38,15 +33,10 @@ def clone_repo(url: str) -> str:
     The given repo will be cloned to current dir.
     """
     logger.info(f"cloning {url}")
-    path = Path.cwd() / "github"
-    os.makedirs(path, exist_ok=True)
-    target_path = str(path / os.path.basename(url)).replace(".git", "")
-
-    if os.path.exists(target_path):
-        logger.info("Skipped git clone, repo already exists!")
-    else:
-        cmd = f"git clone {url} {target_path}"
-        subprocess.run(cmd, shell=True)
+    tempdir = Path(tempfile.mkdtemp())
+    target_path = str(tempdir / os.path.basename(url)).replace(".git", "")
+    cmd = f"git clone {url} {target_path}"
+    subprocess.run(cmd, shell=True)
     return target_path
 
 
