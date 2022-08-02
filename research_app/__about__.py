2d65dcb131b9da2a8048eabbd98d55f0feebeee7
Aniket Maurya <theaniketmaurya@gmail.com>
2022-08-02 17:44:35 +0530

    bump version

--- research_app/__about__.py
+++ research_app/__about__.py
@@ -1,9 +1,9 @@
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __author__ = "Lightning et al."
-__author_email__ = "aniket@grid.ai"
+__author_email__ = "aniket@lightning.ai"
 __license__ = "Apache 2.0"
 __copyright__ = f"Copyright (c) 2021-2022, {__author__}."
-__homepage__ = "https://github.com/PyTorchLightning/lightning-template-research-app"
+__homepage__ = "https://github.com/Lightning-AI/LAI-research-template-App"
 __docs__ = (
     "Share your paper 'bundled' with the arxiv link, poster, live jupyter notebook,"
     "interactive demo to try the model and more!"
