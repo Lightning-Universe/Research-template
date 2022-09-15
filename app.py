e3f1bdf854713719a7047858a7c06ec404542996
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2022-09-15 21:49:40 +0530

    Fix import

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from typing import Dict, List, Optional
 
 import lightning as L
-from lightning.app.frontend.web import StaticWebFrontend
+from lightning.app.frontend import StaticWebFrontend
 from poster import Poster
 from rich import print
 from rich.logging import RichHandler
