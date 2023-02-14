8b9aa0fca5ce77811544c7486d65421e3a883409
Jirka <jirka.borovec@seznam.cz>
2023-02-14 10:09:49 +0100

    simplify setup

--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@
 import os
 from importlib.util import module_from_spec, spec_from_file_location
 
+from pkg_resources import parse_requirements
 from setuptools import find_packages, setup
 
 _PATH_ROOT = os.path.dirname(__file__)
+
+
+def _load_requirements(path_dir: str = _PATH_ROOT, file_name: str = "requirements.txt") -> list:
+    reqs = parse_requirements(open(os.path.join(path_dir, file_name)).readlines())
+    return list(map(str, reqs))
 
 
 def _load_py_module(fname, pkg="research_app"):
@@ -16,14 +22,8 @@ def _load_py_module(fname, pkg="research_app"):
 
 
 about = _load_py_module("__about__.py")
-setup_tools = _load_py_module("setup_tools.py")
-long_description = setup_tools._load_readme_description(_PATH_ROOT, homepage=about.__homepage__, ver=about.__version__)
-
-# https://packaging.python.org/discussions/install-requires-vs-requirements /
-# keep the meta-data here for simplicity in reading this file... it's not obvious
-# what happens and to non-engineers they won't know to look in init ...
-# the goal of the project is simplicity for researchers, don't want to add too much
-# engineer specific practices
+with open(os.path.join(_PATH_ROOT, "README.md")) as fo:
+    readme = fo.read()
 
 setup(
     name="research_app",
@@ -35,12 +35,12 @@ setup(
     download_url="https://github.com/PyTorchLightning/lightning",
     license=about.__license__,
     packages=find_packages(exclude=["tests", "docs"]),
-    long_description=long_description,
+    long_description=readme,
     long_description_content_type="text/markdown",
     include_package_data=True,
     zip_safe=False,
     keywords=["deep learning", "pytorch", "AI"],
     python_requires=">=3.6",
     setup_requires=["wheel"],
-    install_requires=setup_tools._load_requirements(_PATH_ROOT),
+    install_requires=_load_requirements(),
 )
